272e6394febf9b26d2f2d87fb2d2a1e5f8cf8654
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-09 17:51:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,4 +34,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
