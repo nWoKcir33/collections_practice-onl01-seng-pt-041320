7e3539e105e721747ebeee7bb39de931ca3a374a
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-09 17:51:30 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+   array.sort
+ end
+ 
+def sort_array_desc(array)
+  array.sort.reverse
+end
 
+def sort_array_char_count(array)
+  array.sort_by {|i| i.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  array.each do |i|
+  i[2] = "$"
+ end
+end
+
+def find_a(array)
+  array.select {|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
